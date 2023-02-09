e7ab547e07c6cb90d0db213e9829a2fc4d6c0d35
Younes <83889813+younes-beep@users.noreply.github.com>
2023-02-09 18:14:32 +0100

    Update index.js
    update how to connect with delevery guy

--- index.js
+++ index.js
@@ -1,38 +1,32 @@
 
 const express = require('express');
 const upload = require('express-fileupload');
-const router = express.Router();
-const webpack = require('webpack');
-const webpackConfig = require('./webpack.config');
 // const compiler = webpack(webpackConfig);
-const passport = require('passport');
-// const nodemailer = require('nodemailer');
-const https = require('https');
-const FacebookStrategy = require('passport-facebook').Strategy;
-const multer = require('multer');
+const nodemailer = require('nodemailer');
 const bodyParser = require('body-parser');
 var Datastore = require('nedb');
-const fs = require('fs');
 const {google} = require('googleapis');
-const readline = require('readline');
 const app = express(); 
 app.use(upload())
 const cors = require('cors');
-const { error } = require('console');
+const { error, info } = require('console');
 const { getMaxListeners } = require('process');
 const { Server } = require('http');
+const { resolve } = require('path');
+const { errorMonitor } = require('events');
+const { copyFile } = require('fs');
 app.use(cors())
 app.use(express.json());
 app.use(bodyParser.json({limit: '1mb'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname+ '/public'));
 app.use(express.static(__dirname+ '/views'));
-const database = new Datastore('database.db');
-database.loadDatabase();
+const position = new Datastore('position.db');
+position.loadDatabase();
 const database2 = new Datastore('database2.db');
 database2.loadDatabase();
-const database3 = new Datastore('database3.db');
-database3.loadDatabase();
+const infoUser = new Datastore('infoUser.db');
+infoUser.loadDatabase();
 // middlware 
 app.set('view engine', 'ejs');
 
@@ -42,7 +36,6 @@ app.get('/', (req,res) => {
 app.get('/Home', (req,res) => {
   res.render('Home')
 });
-
 app.get('/index', (req,res) => {
   res.render('index')
 });
@@ -74,72 +67,9 @@ app.get('/about-us', (req,res) => {
   res.render('about-us')
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
-
-
-app.listen(3000, () => console.log('listening at 3000'));
-
-
-
-const sendEmailToDelivery = (currentUserEmail, deliveryEmail) => {
-  database3.find({ email: currentUserEmail }, (err, docs) => {
-      if (err) throw err;
-      const currentUser = docs[0];
-      // const email = currentUserEmail
-    
-      // Create a transport object
-      let transporter = nodemailer.createTransport({
-          service: 'gmail',
-          auth: {
-              user: 'raymondyounes2@gmail.com', // my username email 
-              pass: 'iqwvmellavcyatyl' // my password email secure  
-          }
-      });
-     
-      // Define the email options
-      let mailOptions = {
-        from: '"Sneakers" <raymondyounes2@gmail.com>', 
-        to: deliveryEmail,
-        subject: 'Sneakers Delivery Information',
-        html: `<!DOCTYPE html>
-        <html>
-        <head>
-        <title>Delivery Information</title>
-        </head>
-        <body>
-        <h1 style="color: #5e9ca0; text-align: center;">Delivery Information</h1>
-        <p style="text-align: center;">Dear Delivery Person,</p>
-        <p style="text-align: center;">Please find the following information for a recent purchase made by ${currentUser.nameUser}.</p>
-        <p style="text-align: center;">The customer's contact information is as follows:</p>
-        <p style="text-align: center;">Name: ${currentUser.nameUser}</p>
-        <p style="text-align: center;">Phone: ${currentUser.phone}</p>
-        <p style="text-align: center;">Email: ${currentUser.email}</p>
-        <p style="text-align: center;">picture: ${currentUser.location}</p>
-        <p style="text-align: center;">picture: ${currentUser.picture}</p>
-        <p style="text-align: center;">Please process and ship the order as soon as possible.</p>
-        <p style="text-align: center;">Thank you for your prompt attention to this matter.</p>
-        <p style="text-align: center;">Sincerely,</p>
-        <p style="text-align: center;">The younes pro Team</p>
-        </body>
-        </html>
-        `
-    };
-      // Send the email
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              console.log(error);
-          } else {
-              console.log('Email sent: ' + info.response);
-          }
-      });
-  });
-}
-const deliveryEmailOne = 'youneshero436@gmail.com';
-const deliveryEmailTwo = 'anotherdeleceryGuy@gmail.com';
-const confirmationGUY = 'confirmguy@gmail.com'
-// sendEmailToDelivery('raymondyounes2@gmail.com',deliveryEmailOne,deliveryEmailTwo); // email of customer, email of delivery guy
-
-// middleware 
 
 
 // save costumer information into database from html5 
@@ -147,17 +77,17 @@ app.post('/', (req,res) => {
   console.log("i got a request from files")
  const nameUser = req.body.name;
  const phone = req.body.phone;
- const photo = req.files.photo;
+//  const photo = req.files.photo;
  const email = req.body.email;
  const userinfo = {
   nameUser:nameUser,
   phone: phone,
   email:email,
-  photo: photo,
+  // photo: photo,
 };
 console.log(userinfo)
 
-  database3.insert(userinfo, function (err, newDoc) {
+  database2.insert(userinfo, function (err, newDoc) {
     if(err) {
       console.log(err)
       console.log("error paramatre")
@@ -165,26 +95,32 @@ console.log(userinfo)
         status: 'error',
         error: 'Error inserting data into database'
       });
-    } else if (newDoc) {
-      // console.log(newDoc)
+      // this is theend point and the insert , method how i was create it you can modify it to make it save data as we excpect in our sendemail func
+
+      console.log(req.body)
+      console.log(req.file)
+    } else if (newDoc || req.file.photo === undefined) {
       res.json({
         status: 'success',
         nameUser:nameUser,
-        photo: photo,
+        // photo: photo,
         phone: phone,
-        location: data.lat,
-        location: data.lon,
         email:email,
       });
     }
   });
 });
 
+
+
+
+
 // take secreenshot from canvas and send it into database 
 app.post('/upload-screenshot', (req, res) => {
   console.log('i got request from screenshot');
   console.log(req.files)
   const data= req.files.screenshot.data;
+  
   const filename= req.files.screenshot.name;
   const timestamp = Date.now();
   const image = {
@@ -219,62 +155,132 @@ app.post('/api', (req, res) => {
   const timestamp = Date.now();
   console.log(data);
   data.timestamp = timestamp;
-  database.insert(data);
-res.json({
-  status: 'succcess',
-  timestamp: timestamp,
-  data: data.lat,
-  data: data,lon,
-});
-app.get('/api', (req,res) => {
-  database.find({},(err,data) => {
-      res.json(data);
-      if (err) {
-          res.end();
-          return;
-      }
+  position.insert(data, (err, newDoc) => {
+    if(err) {
+      console.log(err);
+      res.status(500).json({
+        status:'error',
+        message:'Failed to insert data into the database',
+      });
+
+    } else {
+      console.log('inserted data into the database ', newDoc);
+      res.json({
+        status:'success',
+        timestamp:timestamp,
+        data:newDoc,
+      });
+    }
   });
-});
-});
+})
 
-app.use('/', function (req,res){
-  res.sendFile(__dirname +  '/public/index.html');
-});
-
-app.use('/', function (req,res){
-  res.sendFile(__dirname +  '/public/index2.html');
-});
-
-app.use('/', function (req,res){
-  res.sendFile(__dirname +  '/public/index3.html');
-});
-
-// app.use(require('webpack-dev-middleware')(compiler,{
-//   noInfo: true,
-//   publicPath: webpackConfig.output.publicPath
-// }));
-
-// app.use(require('webpack-hot-middleware')(compiler));
+const getdata = (req,res) => {
+database2.find({}, (err,docs) => {
+console.log(err);
+if(docs){
+  console.log(docs[2]); 
+}
+})
+}
+// getdata()
 
 
+
+const sendEmailToDelivery = async (currentUserEmail, deliveryEmail) => {
  
 
+  const getLocation = () => {
+     return new Promise((resolve, reject) => {
+      position.find({}, (err, docs) => {
+        if (err) reject(err);
+        location = docs[0];
+        resolve(location)
+        return
+        // console.log(`the block scope location  ${location.lat + " // " + location.lon} `)
+        });
+    })
+  }
+  // getLocation()
+ 
+ const getCurrentUser = () => {
+   return new Promise((resolve, reject) => {
+    database2.find({}, (err, docs) => {
+      if (err) reject(err);
+      if (docs[0].length === 0) {
+        reject('no user found')
+      } else if (docs[0].length <= 0) {
+        console.log('no data found')
+      }
+      currentUser = docs;
+      resolve(currentUser)
+      // console.log(currentUser)
+      return currentUser
+      // console.log(`the block-scope log ${currentUser.nameUser}`); // this line is 
+  })
+ })
+ }
+ 
+Promise.all([getCurrentUser(),getLocation()])
+.then((value) => {
+  var location;
+  var currentUser;
+  location = value[1]
+  console.log(location.lon);
+  console.log(location.lat);
+  currentUser = value[0];
+  let userData = currentUser[2]
+  console.log(userData)
 
-//app.post('/upload-screenshot', (req,res)=> {
-  //   console.log('i got request from seconde screenshot');
-  //   const data = req.files.screenshot.data;
-  // //  const timestamp = Date.now();
-  // //  data.timestamp = timestamp;
-  // //   if(req.files) {
-  //    var file = req.files.screenshot
-  //    var filename = file.name
-  //     file.mv('./upload/'+ filename, function (err) {
-  //        if(err){
-  //          res.send(err)
-  //        } else {
-  //          res.send("file uploaded")
-  //        }
-  //      })
-  //   })
-  
-// end api componont
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "raymondyounes2@gmail.com",
+    pass:"iqwvmellavcyatyl"
+  }
+});
+          ////////////////////////////////////////////////////
+            
+          let mailOptions = {
+            from: '"Sneakers" <raymondyounes2@gmail.com>', 
+            to: deliveryEmail,
+            subject: 'Sneakers Delivery Information',
+            html: `<!DOCTYPE html>
+            <html>
+            <head>
+            </head>
+            <body>
+            <h1 style="color: #5e9ca0; text-align: center;">Delivery Information</h1>
+            <p style="text-align: center;">Dear Delivery Person,</p>
+            <p style="text-align: center;">Please find the following information for a recent purchase made by ${userData.nameUser}.</p>
+            <p style="text-align: center;">Name: ${userData.nameUser}</p>
+            <p style="text-align: center;">Phone: ${userData.phone}</p>
+            <p style="text-align: center;">Email: ${userData.email}</p>
+            <p style="text-align: center;"> picture: ${'Test currentUser.photo'}</p>
+            <p style="text-align: center;">The customer's delivery location is as follows:</p>
+            <p style="text-align: center;">Please process and ship the order as soon as possible.</p>
+            <p style="text-align: center;">Thank you for your prompt attention to this matter.</p>
+            <p style="text-align: center;">Sincerely,</p>
+            <p style="text-align: center;">The younes pro Team</p>
+            <br><br>
+            <a href="https://maps.google.com/?q=${location.lat},${location.lon}" target="_blank">
+            <button>View on Google Maps</button>
+            </a>
+            </body>
+            </html>`
+        };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+              return console.log(error);
+          }
+          console.log('Message sent: %s', info.messageId);
+        });
+      })
+  }
+
+  // const deliveryEmailTwo = 'ysexstin@gmail.com';
+  // const confirmationGUY = 'confirmguy@gmail.co'
+  const deliveryEmailOne = 'youneshero436@gmail.com';
+
+  sendEmailToDelivery('raymondyounes2@gmail.com',deliveryEmailOne); // email of customer, email of delivery guy
+
